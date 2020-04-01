109a0380519c50ff1283c90a570c903199142047
Oleksiy Grechnyev <shrike4625@gmail.com>
2020-04-01 18:18:32 +0300

    example5, does not really work

---
+++ example5.cpp
@@ -0,0 +1,230 @@
+// By Oleksiy Grechnyev, IT-JIM
+// Example 5 : Fun with int8 (But I don't think it works, these layers are not supported !)
+// I use here batch of 2
+
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <NvInfer.h>
+#include <NvOnnxParser.h>
+
+#include <cuda_runtime.h>
+
+//======================================================================================================================
+
+class Logger : public nvinfer1::ILogger {
+public:
+    void log(Severity severity, const char *msg) override {
+        using namespace std;
+        string s;
+        switch (severity) {
+            case Severity::kINTERNAL_ERROR:
+                s = "INTERNAL_ERROR";
+                break;
+            case Severity::kERROR:
+                s = "ERROR";
+                break;
+            case Severity::kWARNING:
+                s = "WARNING";
+                break;
+            case Severity::kINFO:
+                s = "INFO";
+                break;
+            case Severity::kVERBOSE:
+                s = "VERBOSE";
+                break;
+        }
+        cerr << s << ": " << msg << endl;
+    }
+};
+//======================================================================================================================
+
+/// Using unique_ptr with Destroy is optional, but beats calling destroy() for everything
+/// Borrowed from the NVidia tutorial, nice C++ skills !
+template<typename T>
+struct Destroy {
+    void operator()(T *t) const {
+        t->destroy();
+    }
+};
+
+//======================================================================================================================
+/// Optional : Print dimensions as string
+std::string printDim(const nvinfer1::Dims & d) {
+    using namespace std;
+    ostringstream oss;
+    for (int j = 0; j < d.nbDims; ++j) {
+        oss << d.d[j];
+        if (j < d.nbDims - 1)
+            oss << "x";
+    }
+    return oss.str();
+}
+//======================================================================================================================
+/// Optional : Print layers of the network
+void printNetwork(const nvinfer1::INetworkDefinition &net) {
+    using namespace std;
+    using namespace nvinfer1;
+    cout << "\n=============\nNetwork info :" << endl;
+
+    cout << "\nInputs : " << endl;
+    for (int i = 0; i < net.getNbInputs(); ++i) {
+        ITensor * inp = net.getInput(i);
+        cout << "input" << i << " , dtype=" << (int)inp->getType() << " , dims=" << printDim(inp->getDimensions()) << endl;
+    }
+
+    cout << "\nLayers : " << endl;
+    cout << "Number of layers : " << net.getNbLayers() << endl;
+    for (int i = 0; i < net.getNbLayers(); ++i) {
+        ILayer *l = net.getLayer(i);
+        cout << "layer" << i << " , name=" << l->getName() << " , type=" << (int)l->getType() << " , IN ";
+        for (int j = 0; j < l->getNbInputs(); ++j)
+            cout <<  printDim(l->getInput(j)->getDimensions()) << " ";
+        cout << ", OUT ";
+        for (int j = 0; j < l->getNbOutputs(); ++j)
+            cout <<  printDim(l->getOutput(j)->getDimensions()) << " ";
+        cout << endl;
+    }
+
+    cout << "\nOutputs : " << endl;
+    for (int i = 0; i < net.getNbOutputs(); ++i) {
+        ITensor * outp = net.getOutput(i);
+        cout << "input" << i << " , dtype=" << (int)outp->getType() << " , dims=" << printDim(outp->getDimensions()) << endl;
+    }
+
+    cout << "=============\n" << endl;
+}
+//======================================================================================================================
+
+/// Parse onnx file and create a TRT engine
+nvinfer1::ICudaEngine *createCudaEngine(const std::string &onnxFileName, nvinfer1::ILogger &logger, int batchSize) {
+    using namespace std;
+    using namespace nvinfer1;
+
+    unique_ptr<IBuilder, Destroy<IBuilder>> builder{createInferBuilder(logger)};
+
+    unique_ptr<INetworkDefinition, Destroy<INetworkDefinition>> network{
+            builder->createNetworkV2(1U << (unsigned) NetworkDefinitionCreationFlag::kEXPLICIT_BATCH)
+    };
+    unique_ptr<nvonnxparser::IParser, Destroy<nvonnxparser::IParser>> parser{
+            nvonnxparser::createParser(*network, logger)};
+
+    if (!parser->parseFromFile(onnxFileName.c_str(), static_cast<int>(ILogger::Severity::kINFO)))
+        throw runtime_error("ERROR: could not parse ONNX model " + onnxFileName + " !");
+
+    // Optional : print network info
+    printNetwork(*network);
+
+    // Are fancy types available ?
+    cout << "platformHasFastFp16 = " << builder->platformHasFastFp16() << endl;
+    cout << "platformHasFastInt8 = " << builder->platformHasFastInt8() << endl;
+
+    // Create Optimization profile and set the batch size
+    IOptimizationProfile *profile = builder->createOptimizationProfile();
+    profile->setDimensions("input", OptProfileSelector::kMIN, Dims2{batchSize, 3});
+    profile->setDimensions("input", OptProfileSelector::kMAX, Dims2{batchSize, 3});
+    profile->setDimensions("input", OptProfileSelector::kOPT, Dims2{batchSize, 3});
+
+    // Set up the config
+    unique_ptr<IBuilderConfig, Destroy<IBuilderConfig>> config(builder->createBuilderConfig());
+    config->addOptimizationProfile(profile);
+    // Int8 quantization with the explicit range
+    config->setFlag(BuilderFlag::kINT8);
+    config->setFlag(BuilderFlag::kSTRICT_TYPES);
+
+    // Set the dynamic range for all layers and input
+    float minRange = 0., maxRange = 17.;
+    cout << "layers = " << network->getNbLayers() << endl;
+    for (int i = 0; i < network->getNbLayers(); ++i) {
+        ILayer *layer = network->getLayer(i);
+        ITensor *tensor = layer->getOutput(0);
+        tensor->setDynamicRange(minRange, maxRange);
+        layer->setPrecision(DataType::kINT8);
+//        layer->setOutputType(0, DataType::kINT8);
+    }
+    network->getInput(0)->setDynamicRange(minRange, maxRange);
+
+    // Build engine
+    return builder->buildEngineWithConfig(*network, *config);
+}
+
+//======================================================================================================================
+/// Run a single inference
+void launchInference(nvinfer1::IExecutionContext *context, cudaStream_t stream, std::vector<float> const &inputTensor,
+                     std::vector<float> &outputTensor, void **bindings, int batchSize) {
+
+    int inputId = 0, outputId = 1; // Here I assume input=0, output=1 for the current network
+
+    // Infer asynchronously, in a proper cuda way !
+    using namespace std;
+    cudaMemcpyAsync(bindings[inputId], inputTensor.data(), inputTensor.size() * sizeof(float), cudaMemcpyHostToDevice,
+                    stream);
+    context->enqueueV2(bindings, stream, nullptr);
+    cudaMemcpyAsync(outputTensor.data(), bindings[outputId], outputTensor.size() * sizeof(float),
+                    cudaMemcpyDeviceToHost, stream);
+}
+
+//======================================================================================================================
+int main() {
+    using namespace std;
+    using namespace nvinfer1;
+
+    // Parse model, create engine
+    Logger logger;
+    logger.log(ILogger::Severity::kINFO, "C++ TensorRT example5 !!! ");
+    logger.log(ILogger::Severity::kINFO, "Creating engine ...");
+    int batchSize = 2;
+    unique_ptr<ICudaEngine, Destroy<ICudaEngine>> engine(createCudaEngine("model2.onnx", logger, batchSize));
+
+    if (!engine)
+        throw runtime_error("Engine creation failed !");
+
+    // Optional : Print all bindings : name + dims + dtype
+    cout << "=============\nBindings :\n";
+    int n = engine->getNbBindings();
+    for (int i = 0; i < n; ++i) {
+        Dims d = engine->getBindingDimensions(i);
+        cout << i << " : " << engine->getBindingName(i) << " : dims=" << printDim(d);
+        cout << " , dtype=" << (int) engine->getBindingDataType(i) << " ";
+        cout << (engine->bindingIsInput(i) ? "IN" : "OUT") << endl;
+    }
+    cout << "=============\n\n";
+
+    // Create context
+    logger.log(ILogger::Severity::kINFO, "Creating context ...");
+    unique_ptr<IExecutionContext, Destroy<IExecutionContext>> context(engine->createExecutionContext());
+    // Very important, you must set batch size here, otherwise you get zero output !
+    context->setBindingDimensions(0, Dims2(batchSize, 3));
+
+    // Create data structures for the inference
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
+    vector<float> inputTensor{0.5, -0.5, 1.0, 0.0, 0.0, 0.0};
+    vector<float> outputTensor(2 * batchSize, -4.9);
+    void *bindings[2]{0};
+    // Alloc cuda memory for IO tensors
+    size_t sizes[] = {inputTensor.size(), outputTensor.size()};
+    for (int i = 0; i < engine->getNbBindings(); ++i) {
+        // Create CUDA buffer for Tensor.
+        cudaMalloc(&bindings[i], sizes[i] * sizeof(float));
+    }
+
+    // Run the inference !
+    cout << "Running the inference !" << endl;
+    launchInference(context.get(), stream, inputTensor, outputTensor, bindings, batchSize);
+    cudaStreamSynchronize(stream);
+    // Must be [ [1.5, 3.5], [-1,-2] ]
+    cout << "y = [";
+    for (int i = 0; i < batchSize; ++i) {
+        cout << " [" << outputTensor.at(2 * i) << ", " << outputTensor.at(2 * i + 1) << "]";
+        if (i < batchSize - 1)
+            cout << ", ";
+    }
+    cout << " ]" << endl;
+
+    cudaStreamDestroy(stream);
+    return 0;
+}
